47f509f91dc8a51ff80ba2418e0ae4bdef3fb4d5
Matvey Voronin <castleclashrip@gmail.com>
2025-09-21 14:10:00 +0300

    overloading operator -> added to iterator, const and noexcept are attached to iterator's methods

--- include/Matrix_iterator.hpp
+++ include/Matrix_iterator.hpp
@@ -12,40 +12,46 @@ template <typename T> struct Matrix_iterator final {
     using iterator_category = std::contiguous_iterator_tag;
     using difference_type = std::ptrdiff_t;
 
-    Matrix_iterator(pointer ptr): ptr_(ptr) {}
+    Matrix_iterator(pointer ptr) noexcept : ptr_(ptr) {}
 
-    const_reference operator*() const {return *ptr_; }
-    reference operator*() {return *ptr_; }
-    Matrix_iterator& operator++() {++ptr_; return *this; }
-    Matrix_iterator& operator--() {--ptr_; return *this; }
+    const_reference operator*() const noexcept {return *ptr_; }
+    reference operator*() noexcept {return *ptr_; }
+    const_pointer operator->() const noexcept {return ptr_; }
+    pointer operator->() noexcept {return ptr_; }
+    Matrix_iterator& operator++() noexcept {++ptr_; return *this; }
+    Matrix_iterator& operator--() noexcept {--ptr_; return *this; }
 
-    Matrix_iterator operator++(int) {
+    Matrix_iterator operator++(int) noexcept {
         auto old = ptr_;
         ++ptr_;
         return old;
     }
 
-    Matrix_iterator operator--(int) {
+    Matrix_iterator operator--(int) noexcept {
         auto old = ptr_;
         --ptr_;
         return old;
     }
 
-    Matrix_iterator operator+(difference_type dif) { return (ptr_ + dif); }
-    Matrix_iterator operator-(difference_type dif) { return (ptr_ - dif); }
+    Matrix_iterator operator+(difference_type dif) const noexcept { return (ptr_ + dif); }
+    friend Matrix_iterator operator+(difference_type dif, const Matrix_iterator& it) noexcept {
+        return it + dif;  
+    }
+
+    Matrix_iterator operator-(difference_type dif) const noexcept { return (ptr_ - dif); }
     
-    Matrix_iterator& operator+=(difference_type dif) {
+    Matrix_iterator& operator+=(difference_type dif) noexcept {
         ptr_ += dif;
         return *this;
     }
 
-    Matrix_iterator& operator-=(difference_type dif) {
+    Matrix_iterator& operator-=(difference_type dif) noexcept {
         ptr_ -= dif;
         return *this;
     }
 
-    auto operator<=>(const Matrix_iterator& rhs) const = default;
-    difference_type operator-(const Matrix_iterator& rhs) { return ptr_ - rhs.ptr_; }
+    auto operator<=>(const Matrix_iterator& rhs) const noexcept = default;
+    difference_type operator-(const Matrix_iterator& rhs) const noexcept { return ptr_ - rhs.ptr_; }
 
 private:
     pointer ptr_;
